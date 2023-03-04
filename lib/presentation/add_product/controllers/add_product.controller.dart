9893bb310cfd89bd33b61d21b5c97d7e971db737
Christ Henry Loha <62457928+CristLoha@users.noreply.github.com>
2023-03-04 14:41:27 +0800

    update

--- lib/presentation/add_product/controllers/add_product.controller.dart
+++ lib/presentation/add_product/controllers/add_product.controller.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
@@ -8,9 +9,11 @@ class AddProductController extends GetxController {
   final TextEditingController codeC = TextEditingController();
   final TextEditingController nameC = TextEditingController();
   final TextEditingController qtyC = TextEditingController();
+  FirebaseFirestore firestore = FirebaseFirestore.instance;
 
   Future<Map<String, dynamic>> addProduct(Map<String, dynamic> data) async {
     try {
+      await firestore.collection("products").add(data);
       return {
         "error": false,
         "message": "Berhasil menambah product.",
