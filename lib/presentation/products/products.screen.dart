4e4711aa8a865d27f3bd20aef9a4178e7623a8d3
Christ Henry Loha <62457928+CristLoha@users.noreply.github.com>
2023-03-08 09:33:11 +0800

    update

--- lib/presentation/products/products.screen.dart
+++ lib/presentation/products/products.screen.dart
@@ -10,14 +10,31 @@ class ProductsScreen extends GetView<ProductsController> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('ProductsScreen'),
+        title: const Text('PRODUCTS'),
         centerTitle: true,
       ),
-      body: const Center(
-        child: Text(
-          'ProductsScreen is working',
-          style: TextStyle(fontSize: 20),
-        ),
+      body: ListView.builder(
+        itemCount: 5,
+        padding: const EdgeInsets.all(20),
+        itemBuilder: (context, index) {
+          return Card(
+            elevation: 5,
+            margin: const EdgeInsets.only(bottom: 20),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(9),
+            ),
+            child: InkWell(
+              onTap: () {},
+              child: Container(
+                padding: EdgeInsets.all(20),
+                height: 100,
+                child: Row(
+                  children: [Text("Data")],
+                ),
+              ),
+            ),
+          );
+        },
       ),
     );
   }
