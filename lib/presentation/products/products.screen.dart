9480cfd6fae817aecc4b529443987501b7f58528
Christ Henry Loha <62457928+CristLoha@users.noreply.github.com>
2023-03-08 10:48:24 +0800

    update

--- lib/presentation/products/products.screen.dart
+++ lib/presentation/products/products.screen.dart
@@ -1,6 +1,10 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
+import 'package:qr_code/infrastructure/navigation/routes.dart';
+import 'package:qr_code/utils/extension/box_extension.dart';
+import 'package:qr_flutter/qr_flutter.dart';
 
 import 'controllers/products.controller.dart';
 
@@ -13,29 +17,66 @@ class ProductsScreen extends GetView<ProductsController> {
         title: const Text('PRODUCTS'),
         centerTitle: true,
       ),
-      body: ListView.builder(
-        itemCount: 5,
-        padding: const EdgeInsets.all(20),
-        itemBuilder: (context, index) {
-          return Card(
-            elevation: 5,
-            margin: const EdgeInsets.only(bottom: 20),
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(9),
-            ),
-            child: InkWell(
-              onTap: () {},
-              child: Container(
-                padding: EdgeInsets.all(20),
-                height: 100,
-                child: Row(
-                  children: [Text("Data")],
-                ),
-              ),
-            ),
-          );
-        },
-      ),
+      body: StreamBuilder<QuerySnapshot<Map<String, dynamic>>>(
+          stream: controller.streamProducts(),
+          builder: (context, snapProduct) {
+            if (snapProduct.connectionState == ConnectionState.waiting) {
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+            return ListView.builder(
+              itemCount: 5,
+              padding: const EdgeInsets.all(20),
+              itemBuilder: (context, index) {
+                return Card(
+                  elevation: 5,
+                  margin: const EdgeInsets.only(bottom: 20),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(9),
+                  ),
+                  child: InkWell(
+                    onTap: () {
+                      Get.toNamed(Routes.detailProduct);
+                    },
+                    child: Container(
+                      padding: const EdgeInsets.all(20),
+                      height: 100,
+                      child: Row(
+                        children: [
+                          Expanded(
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                const Text(
+                                  '539284724',
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                5.heightBox,
+                                Text('Beng-Beng'),
+                                Text('Jumlah: 7')
+                              ],
+                            ),
+                          ),
+                          SizedBox(
+                            height: 50,
+                            width: 50,
+                            child: QrImage(
+                              data: "54859843569",
+                              size: 200,
+                              version: QrVersions.auto,
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                );
+              },
+            );
+          }),
     );
   }
 }
